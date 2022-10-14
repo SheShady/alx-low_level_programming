b63c6f2c874b5dbd202ff4fd32044c0983f3be41
SheShady <celestinegabriellae@gmail.com>
2022-10-14 05:37:00 -0700

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ *Return: Goal Achieved
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
