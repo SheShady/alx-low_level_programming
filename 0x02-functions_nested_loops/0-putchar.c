e2e4711e3cedcf20905af0fa1319052f1c557941
SheShady <celestinegabriellae@gmail.com>
2022-10-18 00:40:58 -0700

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program that prints _putchar, followed by a new line.
+ *
+ * Return: SUCCESS
+ */
+int main(void)
+{
+printf("_putchar");
+printf("\n");
+return (0);
+}
