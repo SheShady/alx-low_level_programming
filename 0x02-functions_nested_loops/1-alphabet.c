d41a8c25b9c8dda92e461aaca31a27e3b30fadd0
SheShady <celestinegabriellae@gmail.com>
2022-10-18 04:17:58 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - print_alphabet
+ *
+ * Return: SUCCESS
+ */
+void print_alphabet(void);
+{
+char c;
+for (c = 'a'; c = 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
