1570eaefb2f673da287d6f3c8d05477ae272356e
SheShady <celestinegabriellae@gmail.com>
2022-10-19 01:18:09 -0700

    Task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,10 +15,9 @@ if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 if (month >= 3 && day >= 60)
 {
 day++;
-{
+}
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
-}
 }
 else
 {
@@ -30,7 +29,6 @@ else
 {
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
-}
 }
 }
 }
